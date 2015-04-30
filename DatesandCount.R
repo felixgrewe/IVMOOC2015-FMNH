dd7e3031b938b726803dda87e6c507b0355d7482
rshiggin <rshiggin@indiana.edu>
2015-04-29 20:49:25 -0400

    delete and replace

---
+++ DatesandCount.R
@@ -0,0 +1,27 @@
+setwd("/N/home/r/s/rshiggin/Karst")
+library(dplyr)
+
+
+#Attach csv file
+aucatalogVars <- read.csv(file="aucatalogVars.csv", head=TRUE, sep=",")
+data.frame(aucatalogVars)
+
+#Create data/month columns
+datetxt <- as.Date(aucatalogVars$AudDate)
+df <- data.frame(date = datetxt,
+                 year = as.numeric(format(datetxt, format = "%Y")),
+                 month = as.numeric(format(datetxt, format = "%m")))
+auditMerged <- cbind(df,aucatalogVars)
+
+#Remove unncessary columns
+auditMerged$AudDate <- NULL
+auditMerged$X <- NULL
+
+#Count cumulative sums
+cumuldata <- sapply(1:length(auditMerged$AudKey),function(i) 
+                   sum(auditMerged$AudKey[i]==auditMerged$AudKey[1:i]))
+cumulfile <- cbind(auditMerged, cumuldata)
+
+#Write to file
+write.csv(catalog03, file = "catalogPrep.csv")
+                 
--- PrepMerge.R
+++ PrepMerge.R
@@ -1,35 +1,24 @@
----
-title: "Field Museum"
----
-```{r}
-setwd("/Users/Richard/Documents/TextAnalysisWithR")
-library(dplyr)
-library(markdown)
-library(rmarkdown)
-library(knitr)
-library(yaml)
-```
 
-####Attach csv files
-```{r}
+setwd("/N/home/r/s/rshiggin/Karst")
+library(dplyr)
+
+#Attach csv files
 eaudit01 <- read.csv(file="MOOCeaudit03.csv", head=TRUE, sep=",")
 catalog <- read.csv(file="ChinaHall_masterRev.csv", head=TRUE, sep=",")
-```
 
-####Select variables
-```{r}
+#Select table variables
 eaudit02 <- eaudit01[,c(3, 4, 6, 7, 8)]
 aucatalog <- catalog[,c(3, 5)]
-```
 
-####Join tables
-```{r}
+#Join tables
 merged <- merge(eaudit02, aucatalog)
-```
 
-####Filter admininstrative user
-```{r}
+#Filter admininstrative user
 aucatalogVars <- merged[merged$AudUser != "emu", ]
 summary(aucatalogVars)
-```
+
+#Write to file
+write.csv(aucatalogVars, file = "aucatalogVars.csv")
+
+
 
