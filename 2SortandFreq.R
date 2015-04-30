a33a4a7746dd259daea1610061bf07a4abfaf57c
rshiggin <rshiggin@users.noreply.github.com>
2015-04-30 00:08:05 -0400

    Minor edits

--- 2SortandFreq.R
+++ 2SortandFreq.R
@@ -1,11 +1,12 @@
 setwd("/Users/Richard/Documents/TextAnalysisWithR")
 library(dplyr)
+library(tidyr)
 
 #Attach csv file
 aucatalogFile <- read.csv(file="aucatalogFile.csv", head=TRUE, sep=",")
 data.frame(aucatalogFile)
 
-#Create data/month columns
+#Create date/month columns
 datetxt <- as.Date(aucatalogFile$AudDate)
 df <- data.frame(date = datetxt,
                  year = as.numeric(format(datetxt, format = "%Y")),
@@ -14,36 +15,22 @@ auditMerged <- cbind(df,aucatalogFile)
 tbl_df(auditMerged)
 write.csv(auditMerged, file = "auditMerged.csv")
 
-#Count cumulative sums with SQL query
+#Count cumulative sums
 df <- read.csv(file="auditMerged.csv", head=TRUE, sep=",")
 cumulData <- df %>% 
   group_by(year, month, AudKey, CAT_SecDepartment_tab) %>% 
   summarise(freqall = n())
-
-#Write to file
 write.csv(cumulData, file = "cumulData.csv")
 
-
-#Relative frequency
-library(dplyr)
-library(tidyr)
+#Relative frequencies
 cumulData <- read.csv(file="cumulData.csv", head=TRUE, sep=",")
 freq <- cumulData %>%
   group_by(AudKey, year, month) %>%
   summarise (n = n()) %>%
   mutate(freq = n / sum(n) * 100)
-
 write.csv(freq, file = "freq.csv")
 
-#Sort by highest cumulative
+#Sort by cumulative descending
 sortTop <- arrange(cumulData, desc(freqall))
 sortTop$X <- NULL
-
-
-sort2 <- data.frame(cumulData) %>%
-  group_by(AudKey, freqall, year, month, CAT_SecDepartment_tab) %>%
-  arrange(freqall, desc=TRUE) 
-
 write.csv(topN, file = "topN.csv")
-
-
